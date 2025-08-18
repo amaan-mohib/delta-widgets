7226bc32c004153b02016af05bdd0c5431cde630
amaan-mohib <amaan.mohib@hotmail.com>
2025-08-19 03:37:06 +0530

    feat: migrate scripts to CommonJS modules

--- scripts/bump-version.cjs
+++ scripts/bump-version.cjs
@@ -5,8 +5,8 @@ const pkg = JSON.parse(fs.readFileSync("package.json", "utf-8"));
 const tauriConfPath = path.join("src-tauri", "tauri.conf.json");
 const tauriConf = JSON.parse(fs.readFileSync(tauriConfPath, "utf-8"));
 
-if (tauriConf.package.version !== pkg.version) {
-  tauriConf.package.version = pkg.version;
+if (tauriConf.version !== pkg.version) {
+  tauriConf.version = pkg.version;
   fs.writeFileSync(tauriConfPath, JSON.stringify(tauriConf, null, 2));
   console.log(`🔄 Synced tauri.conf.json to version ${pkg.version}`);
 } else {
